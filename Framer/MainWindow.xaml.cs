9276215b38c1a2ed233e4976c464bc17e0330107
Igor Shapiro <igor@licensario.com>
2011-10-07 03:19:04 +0200

    Initial project Displays a hard-coded image & scales it using the slider

---
+++ Framer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Framer.Model;
+
+namespace Framer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+            icList.DataContext = GetImagesList();
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            DataContext = GetImagesList();
+        }
+
+        public ImagesListModel GetImagesList() {
+            return new ImagesListModel
+                   {
+                       Images = new List<ImageInfoModel>
+                                {
+                                    new ImageInfoModel
+                                    {
+                                        Path = @"C:\Users\Public\Pictures\Sample Pictures\Chrysanthemum.jpg"
+                                    }
+                                }
+                   };
+        }
+
+        private void OnFileExitClick(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void OnFileOptionsClick(object sender, RoutedEventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void OnImagesListDragEnter(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.Copy;
+        }
+    }
+}
---
+++ Framer/Model/ImagesListModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Framer.Model
+{
+    public class ImagesListModel {
+        public IList<ImageInfoModel> Images { get; set; }
+        public double Width { get; set; }
+
+        public ImagesListModel() {
+            Width = 200;
+        }
+    }
+
+    public class ImageInfoModel {
+        public string Path { get; set; }
+    }
+}
